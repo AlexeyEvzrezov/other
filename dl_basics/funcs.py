8a1de5e69bcfcde998aafb5bd91c42e7283188b7
Alexey Evzrezov <alexeyevzrezov@gmail.com>
2021-07-27 16:51:46 +0300

    Add dl_basics

--- dl_basics/funcs.py
+++ dl_basics/funcs.py
@@ -24,9 +24,7 @@ class SoftmaxRegression(object):
     def __init__(self, in_features, out_features):
         self.W = torch.normal(0, 0.01, (in_features, out_features), requires_grad=True)
         self.b = torch.zeros(1, requires_grad=True)
-
-    def params(self):
-        return [self.W, self.b]
+        self.params = [self.W, self.b]
 
     def __call__(self, X):
         logits = X.reshape(-1, self.W.shape[0]) @ self.W + self.b
@@ -69,5 +67,6 @@ def train(model, loss_fn, dataloader, optimizer, n_epochs):
         for X, y in dataloader:
             y_hat = model(X)
             loss = loss_fn(y_hat, y)
+            print(loss.mean())
             loss.mean().backward()
             optimizer.step()
