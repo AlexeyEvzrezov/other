7d56b7d7fdee2b81c126aea93ea676c81f41ff88
Alexey Evzrezov <alexeyevzrezov@gmail.com>
2021-07-29 19:49:37 +0300

    Add dl_basics

--- dl_basics/regularization.py
+++ dl_basics/regularization.py
@@ -3,3 +3,13 @@ import torch
 
 def l2_reg(w):
     return torch.sum(w ** 2) # / 2
+
+
+def dropout(layer, p_drop):
+    assert 0 <= p_drop <= 1
+    if p_drop == 0:
+        return layer
+    if p_drop == 1:
+        return torch.zeros_like(layer)
+    mask = (torch.rand(layer.shape) > p_drop).float()
+    return mask * layer / (1.0 - p_drop)
